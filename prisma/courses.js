c715ec13f31c9ebd55b189dd6de66223b3873bcc
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-01 12:29:07 +0600

    prisma added

---
+++ prisma/courses.js
@@ -0,0 +1,16 @@
+import prisma from './prisma';
+
+// get all courses
+export const getAllCourses = async () => {
+  const courses = await prisma.course.findMany({});
+
+  return courses;
+};
+
+// get single course
+export const getSingleCourse = async (id) => {
+  const course = await prisma.course.findUnique({
+    where: { id: id },
+  });
+  return course;
+};
