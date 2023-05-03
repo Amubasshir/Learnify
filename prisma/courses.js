e6c56453b5d8e180d807e5f420742d1317affb82
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-03 12:06:39 +0600

    course api added

--- prisma/courses.js
+++ prisma/courses.js
@@ -10,7 +10,8 @@ export const getAllCourses = async () => {
 // get single course
 export const getSingleCourse = async (id) => {
   const course = await prisma.course.findUnique({
-    where: { id: id },
+    where: { id },
   });
+
   return course;
 };
