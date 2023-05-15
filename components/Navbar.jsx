9afa511f02fae285f7c43e67c952351acca88686
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-15 07:31:35 +0600

    navbar done

---
+++ components/Navbar.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div className="navbar h-20 bg-black text-gray-400 flex items-center">
+      <div className="wrapper flex justify-between">
+        <Link href="/" className="text-white font-semibold">
+          Learnify
+        </Link>
+        <div className="flex gap-5">
+          <Link href="/" className="hover:text-white transition-colors">
+            Home
+          </Link>
+          <Link href="/courses" className="hover:text-white transition-colors">
+            Courses
+          </Link>
+          <Link href="/about" className="hover:text-white transition-colors">
+            About
+          </Link>
+          <Link href="/contact" className="hover:text-white transition-colors">
+            Contact
+          </Link>
+        </div>
+        <div>
+          <Link href="/">Sign in</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import 'componemt/styles/globals.css'
+import Footer from 'componemt/components/Footer';
+import Navbar from 'componemt/components/Navbar';
+import 'componemt/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
