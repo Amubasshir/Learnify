f0adc469e20edfafffc9475ef8cb686eb0308543
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-16 08:17:43 +0600

    course details page added

--- components/CourseItem.jsx
+++ components/CourseItem.jsx
@@ -6,51 +6,59 @@ import Button from './Button';
 const CourseItem = ({ course }) => {
   return (
     <div className="w-full lg:w-[30rem] shadow-md rounded-md overflow-hidden">
-      <div className="item-img w-full object-cover h-[21rem] lg:h-[20rem] overflow-hidden">
+      <div className="w-full h-[25rem] lg:h-[20rem] overflow-hidden">
         <Image
           src={course.cover}
           alt={course.title}
           width={640}
-          height={320}
-          className="w-full h-full object-cover"
+          height={360}
           priority
+          className="w-full h-full object-cover"
         />
       </div>
-      <div className="item-text p-5 space-y-2">
-        <h3 className="text-3xl lg:text-2xl font-medium ">{course.title}</h3>
-        <p>{course.description}</p>
-        <p className="flex gap-5 justify-between text-gray-600">
-          <span>
+
+      <div className="p-5 ">
+        <h3 className="text-3xl lg:text-2xl font-medium">{course.title}</h3>
+
+        <p className="flex justify-between text-gray-500 pb-1">
+          <span className="text-black">
             by{' '}
             <span className="text-black font-semibold">
               {course.instructor}
             </span>
           </span>
-          <span className="text-black">duration: {course.duration}</span>
+          <span className="text-black">
+            Duration:{' '}
+            <span className="text-black font-semibold">{course.duration}</span>
+          </span>
         </p>
-        <div className="flex gap-5">
-          <p className="flex items-center gap-5">
-            <span className="flex items-center">
-              {course.rating}
-              <span className="flex pl-1 text-orange-400">
-                <AiOutlineStar /> <AiOutlineStar />
-                <AiOutlineStar />
-                <AiOutlineStar />
-              </span>
-            </span>
-            <span className="text-black">Students: {course.students}</span>
+
+        <p className="flex justify-between text-gray-500 pb-2">
+          <span className="text-black">
+            Enrolled students:{' '}
+            <span className="text-black font-semibold">{course.students}</span>
+          </span>
+          <span className="flex items-center gap-1">
+            <AiOutlineStar className="text-orange-500" />
+            <AiOutlineStar className="text-orange-500" />
+            <AiOutlineStar className="text-orange-500" />
+            <AiOutlineStar className="text-orange-500" />
+            <span className="text-black font-semibold">{course.rating}</span>
+          </span>
+        </p>
+
+        <p
+          className="text-gray-500 text-base leading-5
+         font-light tracking-tighter pb-6"
+        >
+          {course.description.substring(0, 100)}...
+        </p>
+
+        <div className="flex justify-between items-center">
+          <p className="text-lg font-semibold">
+            {currencyConverter(course.price, 'en-GB', 'EUR')}
           </p>
-          <p className="">
-            Price: {currencyConverter(course.price, 'en-GB', 'EUR')}
-          </p>
-        </div>
-        <div>
-          <Button
-            href={`/courses/${course.id}`}
-            placeholder="View Course"
-            color="primary"
-            size="full"
-          />
+          <Button href={`/courses/${course.id}`} placeholder="View Details" />
         </div>
       </div>
     </div>
