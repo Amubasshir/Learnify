0d4e54da6db7761a0c1ac998a4c91b0d4e357fb9
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-18 11:13:08 +0600

    course details done

--- pages/courses/[courseid].js
+++ pages/courses/[courseid].js
@@ -1,18 +1,36 @@
+import Button from 'componemt/components/Button';
 import { getSingleCourse } from 'componemt/prisma/courses';
+import { currencyConverter } from 'componemt/utils/currencyconverter';
 
 const CourseDetails = ({ course }) => {
   return (
-    <div className="course-details wrapper py-10">
+    <div className="course-details wrapper py-10 min-h-screen">
       <div
         style={{ backgroundImage: `url(${course.cover})` }}
-        className="w-full sm:h-[30rem] h-[20rem] bg-no-repeat bg-cover bg-center"
+        className="w-full h-[30rem] bg-no-repeat bg-cover bg-center"
       />
-      <div className="mt-10">
-        <div className="left">
-          <h2 className="text-4xl">{course.title}</h2>
-          <p>Instructor: {course.instructor}</p>
+      <div className="mt-10 grid lg:grid-cols-2 lg:gap-10 space-y-2 lg:space-y-0">
+        <div className="left space-y-2">
+          <h2 className="text-4xl md:text-2xl">{course.title}</h2>
+          <p className="font-medium">Instructor: {course.instructor}</p>
+          <p>{course.description}</p>
         </div>
-        <div className="right"></div>
+        <div className="right space-y-2">
+          <p>
+            <span className="font-semibold">Duration:</span> {course.duration}
+          </p>
+          <p>
+            <span className="font-semibold">Students:</span> {course.students}
+          </p>
+          <p>
+            <span className="font-semibold">Rating:</span> {course.rating}
+          </p>
+          <p>
+            <span className="font-semibold">Price:</span>
+            {currencyConverter(course.price, 'en-GB', 'EUR')}
+          </p>
+          <Button href="/checkout" placeholder="Enroll Now"></Button>
+        </div>
       </div>
     </div>
   );
