50f4a7ca8dcccbb9c89c9ecae819efd4f7c67f35
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-18 15:56:31 +0600

    google provider done

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import Footer from 'componemt/components/Footer';
 import Navbar from 'componemt/components/Navbar';
 import 'componemt/styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, session }) {
   return (
     <>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
+      <SessionProvider session={session}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </SessionProvider>
     </>
   );
 }
--- pages/users/login.jsx
+++ pages/users/login.jsx
@@ -1,5 +1,51 @@
-const LoginPage = () => {
-  return <div>LoginPage</div>;
+import SectionTitle from 'componemt/components/SectionTitle';
+import { getSession, signIn } from 'next-auth/react';
+import { FcGoogle } from 'react-icons/fc';
+
+const LoginPage = ({ session }) => {
+  console.log(session);
+  const loginWithGoogle = async () => {
+    try {
+      await signIn('google');
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  return (
+    <div className="login wrapper py-10 min-h-screen">
+      <SectionTitle
+        span={'Login'}
+        h2={'Get Started With Google'}
+        p={'Please login to continue our latest features'}
+      />
+
+      <div className="flex justify-center">
+        <button
+          onClick={loginWithGoogle}
+          className={
+            'flex gap-2 items-center bg-black text-white py-3 px-6 rounded-lg font-medium mt-10 hover:bg-gray-700 duration-300'
+          }
+        >
+          <span className="text-2xl">
+            <FcGoogle />
+          </span>
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+  if (session) {
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
