fa10f8699d06488b89d72594313748a6e5e802b2
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-19 10:16:38 +0600

    checkout page added

--- pages/checkout/[courseid].js
+++ pages/checkout/[courseid].js
@@ -1,5 +1,45 @@
-const Checkout = () => {
+import { getSingleCourse } from 'componemt/prisma/courses';
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+
+const Checkout = ({ course }) => {
+  const { data: session } = useSession();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    mobile: '',
+    address: '',
+    courseTitle: course.title,
+    price: course.price,
+  });
+
+  useEffect(() => {
+    if (session) {
+      setFormData((prev) => ({
+        ...prev,
+        name: session.user.name,
+        email: session.user.email,
+      }));
+    }
+  }, [session]);
+
   return <div>[courseid]</div>;
 };
 
 export default Checkout;
+
+export const getServerSideProps = async ({ query }) => {
+  const course = await getSingleCourse(query.courseid);
+
+  const updatedCourse = {
+    ...course,
+    updatedAt: course.updatedAt.toString(),
+    createdAt: course.createdAt.toString(),
+  };
+  console.log(updatedCourse);
+  return {
+    props: {
+      course: updatedCourse,
+    },
+  };
+};
